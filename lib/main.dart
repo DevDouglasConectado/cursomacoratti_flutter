45b821bbd09250fd4f31e5e897e948084f0830d5
DevDouglasConectado <douglasrodriguesconectadoifmg@gmail.com>
2021-04-14 20:31:30 -0300

    widget scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -5,11 +5,49 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: "Aula03",
-      home: new Container(
-        color: Colors.blue,
+      theme: ThemeData(
+        primaryColor: Colors.blue
       ),
+      home: meuWidget()
     );
+  }
+
+   meuWidget() {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Flutter - 2019"),
+        centerTitle: true,
+      ),
+      body: Container(
+        color: Colors.white,
+      ),
+      drawer: Container(
+        color: Colors.orange,
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add_a_photo),
+        onPressed: (){print("Pressionado");},
+       ),
+       bottomNavigationBar: BottomAppBar(
+         child: Container(
+           height: 40.0,
+           child: Row(
+             children: [
+               Text("Meu bottomBar"),
+               Icon(Icons.home)
+              ],
+           ),
+         ),
+         color: Colors.lime,
+      ),
+      persistentFooterButtons: [
+        IconButton( icon: Icon(Icons.add_alarm),
+        onPressed: null),
+      ],
+    
+      );
   }
 }
 
