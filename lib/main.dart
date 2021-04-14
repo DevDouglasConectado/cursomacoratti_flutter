44aaeacb6ab865c679d114ebda43fadd6adf0d11
DevDouglasConectado <douglasrodriguesconectadoifmg@gmail.com>
2021-04-14 17:59:50 -0300

    commit termino aula 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,27 @@
 import 'package:flutter/material.dart';
 
 void main() {runApp(
-  Center(
-    child: Text("ola flutter",
-    textDirection: TextDirection.ltr,
-       style: TextStyle(
-         fontSize: 40.0 
-       )
-     
-    ),
-  )
+  MyApp()
 );
+
+}
+
+class MyApp extends StatelessWidget {
+  //*const MyApp({
+    //Key key}) : super(key: key);contrutor vazio 
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("ola flutter",
+      textDirection: TextDirection.ltr,
+         style: TextStyle(
+           fontSize: 40.0,
+           fontWeight: FontWeight.bold
+         )
+       
+      ),
+    );
+  }
 }
 
