b5df022c1813a617bc46cf8d69cce18158206d21
DevDouglasConectado <douglasrodriguesconectadoifmg@gmail.com>
2021-04-14 19:03:38 -0300

    materialApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,21 @@
 import 'package:flutter/material.dart';
-
-void main() {runApp(
-  MyApp()
-);
-
-}
-
+void main()=> runApp(MyApp()); 
+  
 class MyApp extends StatelessWidget {
-  //*const MyApp({
-    //Key key}) : super(key: key);contrutor vazio 
-
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("ola flutter",
-      textDirection: TextDirection.ltr,
-         style: TextStyle(
-           fontSize: 40.0,
-           fontWeight: FontWeight.bold
-         )
-       
+    return MaterialApp(
+      title: "Aula03",
+      home: new Container(
+        color: Colors.blue,
       ),
     );
   }
 }
+
+
+
+
+
+
 
