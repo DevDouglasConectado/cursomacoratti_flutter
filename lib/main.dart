be0529fdfe253fb667db16bbbe3dd5990f27fd67
DevDouglasConectado <douglasrodriguesconectadoifmg@gmail.com>
2021-04-14 22:20:32 -0300

    widgetButton

--- lib/main.dart
+++ lib/main.dart
@@ -6,49 +6,32 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: "Aula03",
+      title: "Widgets Básicos",
       theme: ThemeData(
         primaryColor: Colors.blue
       ),
-      home: meuWidget()
-    );
+      home: widgetsBasicos());
   }
-
-   meuWidget() {
+}
+class widgetsBasicos extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Flutter - 2019"),
-        centerTitle: true,
-      ),
+        title: Text("Widgets Básicos"),
+        ),
       body: Container(
-        color: Colors.white,
-      ),
-      drawer: Container(
-        color: Colors.orange,
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add_a_photo),
-        onPressed: (){print("Pressionado");},
-       ),
-       bottomNavigationBar: BottomAppBar(
-         child: Container(
-           height: 40.0,
-           child: Row(
-             children: [
-               Text("Meu bottomBar"),
-               Icon(Icons.home)
-              ],
-           ),
-         ),
-         color: Colors.lime,
-      ),
-      persistentFooterButtons: [
-        IconButton( icon: Icon(Icons.add_alarm),
-        onPressed: null),
-      ],
-    
-      );
-  }
+        color: Colors.red,
+        child: widgetButton()
+                )
+            );
+            
+          
+          }
+        
+          widgetButton() {}
+
+  
 }
 
 
