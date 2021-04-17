eaaa970e0257fd5214946377bff6ffccaec8d64c
DevDouglasConectado <douglasrodriguesconectadoifmg@gmail.com>
2021-04-16 23:22:03 -0300

    commit stateful

--- lib/main.dart
+++ lib/main.dart
@@ -9,82 +9,57 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: "Widgets Básicos",
+      title: "Cor preferida",
       theme: ThemeData(
         primaryColor: Colors.blue
       ),
-      home: widgetsLogin());
+      home: widgetCorPreferida());
   }
 }
-       class widgetsLogin extends StatelessWidget {
-         TextStyle style = TextStyle(fontFamily: 'Montserrat', fontSize: 20.0);
-         @override
-         Widget build(BuildContext context) {
+class widgetCorPreferida extends StatefulWidget {
+  @override
+  _widgeCorPreferidaState createState() => _widgeCorPreferidaState();
+}
 
-           final emailField = TextField(
-             obscureText: false,
-             style: style,
-             decoration: InputDecoration(
-               contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-               hintText: "Email",
-               border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
-             );
+class _widgeCorPreferidaState extends State<widgetCorPreferida> {
+  String nomeCor = "";
+  @override
+  Widget build(BuildContext context) {
 
+    debugPrint("#### widget foi criado ");
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("StatefulWidget"),
+      ),
+      body: Container(
+        margin: EdgeInsets.all(20.0),
+        //color: Colors.yellow,
+        child: Column(
+          children: [
+          TextField(
+            onSubmitted:(String texto){
+              setState(() {
+                debugPrint("#### setStade foi chamado -> redesenhar o widget-----------");
+                nomeCor = texto;
+              });
 
-             final passwordField = TextField(
-             obscureText: true,
-             style: style,
-             decoration: InputDecoration(
-               contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-               hintText: "Password",
-               border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
-             );
-              
-
-             final buttonLogin = ButtonTheme(
-                    minWidth: MediaQuery.of(context).size.width,
-                    padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-                    child: RaisedButton(
-                          color: Color(0Xff01A0C7),
-                          child: Text("Login",textAlign: TextAlign.center,
-                          style: style.copyWith(
-                            color:Colors.white,
-                            fontWeight: FontWeight.bold)),
-                        onPressed: (){},
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(30.0)
-                        ),
-                    ),
-
-             ); 
-              
-                return Scaffold(body: Center(
-                  child: Container(
-                    color: Colors.white,
-                    padding: const EdgeInsets.all(40.0),
-                      child: Column(
-                  children: [
-                          SizedBox(height:150.0, 
-                          child:Image.asset("imagens/liga.jpg", fit: BoxFit.contain,)
-                          ),
-                          SizedBox(height: 40.0),emailField,
-                          SizedBox(height:20.0),passwordField,
-                          SizedBox(height:30.0),buttonLogin,
-
-                     ],
-
-                        )
+              nomeCor = texto;
+            }
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Text("A sua cor preferida é : $nomeCor ",
+            style: TextStyle(fontSize: 20.0) ),
+          )
 
 
-
-                      
-                  )),
-             
-           );
-         }
-       } 
           
-        
+        ],),),
+      
+    );
+  }
+}
+       
          
             
 
