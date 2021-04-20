5b2a3a3b2f25c9e222635d7ff0331da8d34049da
DevDouglasConectado <douglasrodriguesconectadoifmg@gmail.com>
2021-04-19 21:11:45 -0300

    listView

--- lib/main.dart
+++ lib/main.dart
@@ -1,65 +1,60 @@
-//import 'package:cursomacoratti_flutter/widget_raisedbutton.dart';
-import 'dart:ui';
+//import 'package:cursomacoratti_flutter/widget_raisedbutton.da
 
 import 'package:flutter/material.dart';
-void main()=> runApp(MyApp()); 
+ 
+ void main() => runApp(MyApp());
+
+ class MyApp extends StatelessWidget {
+   @override
+   Widget build(BuildContext context) {
+     return MaterialApp(
+       home: Scaffold(
+         appBar: AppBar(
+           title: Text("Lista Básica"),
+           centerTitle: true,
+         ),
+         body: buildListView(),
+       ),
+       );
+   }
+     buildListView(){
+         return ListView(
+           children: [
+                      ListTile(
+                        leading: Icon(Icons.add_location),
+                        title: Text("Texto"),
+                        subtitle: Text("Meu texto"),
+                        trailing: Icon(Icons.add_shopping_cart),
+                        selected: true,
+                         onTap: (){
+                           print("ok");
+                         },
+
+                      ),
+
+                      ListTile(
+                        leading: Icon(Icons.add_to_photos),
+                        title: Text("Texto"),
+                        subtitle: Text("Meu texto"),
+                        trailing: Icon(Icons.add_box),
+                        enabled: true,
+                        onLongPress: (){
+                          print("");
+                        },
+                      ),
+                   Text("Texto de exemplo"),
+                   Container(color: Colors.green, height: 60.0,)
+           ],
+              
+
+         );
+     }
+
+
+   }
+ 
   
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: "Cor preferida",
-      theme: ThemeData(
-        primaryColor: Colors.blue
-      ),
-      home: widgetCorPreferida());
-  }
-}
-class widgetCorPreferida extends StatefulWidget {
-  @override
-  _widgeCorPreferidaState createState() => _widgeCorPreferidaState();
-}
 
-class _widgeCorPreferidaState extends State<widgetCorPreferida> {
-  String nomeCor = "";
-  @override
-  Widget build(BuildContext context) {
-
-    debugPrint("#### widget foi criado ");
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("StatefulWidget"),
-      ),
-      body: Container(
-        margin: EdgeInsets.all(20.0),
-        //color: Colors.yellow,
-        child: Column(
-          children: [
-          TextField(
-            onSubmitted:(String texto){
-              setState(() {
-                debugPrint("#### setStade foi chamado -> redesenhar o widget-----------");
-                nomeCor = texto;
-              });
-
-              nomeCor = texto;
-            }
-          ),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Text("A sua cor preferida é : $nomeCor ",
-            style: TextStyle(fontSize: 20.0) ),
-          )
-
-
-          
-        ],),),
-      
-    );
-  }
-}
-       
          
             
 
---
+++ lib/stateful_widget.dart
@@ -0,0 +1,58 @@
+/*import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Cor preferida",
+      theme: ThemeData(
+        primaryColor: Colors.blue
+      ),
+      home: widgetCorPreferida());
+  }
+}
+class widgetCorPreferida extends StatefulWidget {
+  @override
+  _widgeCorPreferidaState createState() => _widgeCorPreferidaState();
+}
+
+class _widgeCorPreferidaState extends State<widgetCorPreferida> {
+  String nomeCor = " ";
+  @override
+  Widget build(BuildContext context) {
+
+    debugPrint("#### widget foi criado ");
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("StatefulWidget"),
+      ),
+      body: Container(
+        margin: EdgeInsets.all(20.0),
+        //color: Colors.yellow,
+        child: Column(
+          children: [
+          TextField(
+            onSubmitted:(String texto){
+              setState(() {
+                debugPrint("#### setStade foi chamado -> redesenhar o widget-----------");
+                nomeCor = texto;
+              });
+
+              nomeCor = texto;
+            }
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Text("A sua cor preferida é : $nomeCor ",
+            style: TextStyle(fontSize: 20.0) ),
+          )
+
+
+          
+        ],),),
+      
+    );
+  }
+}*/
+       
